f3f3642581d2e8431ce804049def87a223378be9
soy <soykeepgoing@gmail.com>
2025-08-18 14:00:38 +0900

    업데이트: [백준] 9466번: 텀프로젝트

--- boj/dfs/9466.py
+++ boj/dfs/9466.py
@@ -2,27 +2,27 @@ import sys
 sys.setrecursionlimit(10**5)
 input = sys.stdin.readline
 
-def solve_case():
-    in_team = [False] * m 
+def solution():
     state = [0] * m 
+    in_team = [False] * m
 
     def dfs(node):
         state[node] = 1
         next_node = students[node]
         if state[next_node] == 0:
             dfs(next_node)
-        elif state[next_node] == 1: # 순환을 발견 (처리 중인 상태이므로)
+        elif state[next_node] == 1:
             cur = next_node
             while True:
                 in_team[cur] = True 
                 cur = students[cur]
                 if cur == next_node:
                     break 
-        state[node] = 2
+        state[node] = 2         
 
     for i in range(m):
-        dfs(i)
-
+        if state[i] == 0:
+            dfs(i)
     return m - sum(in_team)
 
 def minus(x):
@@ -34,5 +34,5 @@ if __name__ == '__main__':
         m = int(input())
         students = list(map(int, input().split()))
         students = list(map(minus, students))
-        answer = solve_case()
+        answer = solution()
         print(answer)
